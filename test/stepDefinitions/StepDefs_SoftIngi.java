5b131f186c599ab82062d3bf9f8b9bb9ba781d40
chris <chris@192.168.0.158>
2020-05-04 17:39:27 +0200

    kys

---
+++ test/stepDefinitions/StepDefs_SoftIngi.java
@@ -0,0 +1,114 @@
+package stepDefinitions;
+
+public class StepDefs_SoftIngi {
+
+//	@Given("<Bruger er logget ind>")
+//	public void bruger_er_logget_ind() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren opretter et nyt projekt>")
+//	public void brugeren_opretter_et_nyt_projekt() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren forsoeger at oprette et nyt projekt, paa et tidspunkt, hvor der stadig er ledige numre paa formaten|\\(y)aarstal, \\(n)loebenummer|: yynnn>")
+//	public void brugeren_forsoeger_at_oprette_et_nyt_projekt_paa_et_tidspunkt_hvor_der_stadig_er_ledige_numre_paa_formaten_y_aarstal_n_loebenummer_yynnn() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Then("<Systemet opretter et nyt projekt og tildeler det et projekt nummer. Dette nummer informeres brugeren om>")
+//	public void systemet_opretter_et_nyt_projekt_og_tildeler_det_et_projekt_nummer_Dette_nummer_informeres_brugeren_om() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Given("<Bruger er logget ind>")
+//	public void bruger_er_logget_ind() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren opretter et nyt projekt>")
+//	public void brugeren_opretter_et_nyt_projekt() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren forsoeger at oprette et nyt projekt, paa et tidspunkt, hvor der ikke ledige numre paa formaten|\\(y)aarstal, \\(n)loebenummer|: yynnn>")
+//	public void brugeren_forsoeger_at_oprette_et_nyt_projekt_paa_et_tidspunkt_hvor_der_ikke_ledige_numre_paa_formaten_y_aarstal_n_loebenummer_yynnn() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Then("<Systemet opretter ikke et nyt projekt og informere brugeren om, at der ikke er flere ledige Loebenumre for aarstallet>")
+//	public void systemet_opretter_ikke_et_nyt_projekt_og_informere_brugeren_om_at_der_ikke_er_flere_ledige_Loebenumre_for_aarstallet() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Given("<Bruger er logget ind>")
+//	public void bruger_er_logget_ind() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren opretter et nyt projekt>")
+//	public void brugeren_opretter_et_nyt_projekt() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren forsoeger at oprette et nyt projekt, paa et tidspunkt, hvor der stadig er ledige numre paa formaten|\\(y)aarstal, \\(n)loebenummer|: yynnn>")
+//	public void brugeren_forsoeger_at_oprette_et_nyt_projekt_paa_et_tidspunkt_hvor_der_stadig_er_ledige_numre_paa_formaten_y_aarstal_n_loebenummer_yynnn() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren angivet et projektnavn>")
+//	public void brugeren_angivet_et_projektnavn() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Then("<Systemet opretter et nyt projekt og tildeler det et projekt nummer og et navn. Bruger informeres herom>")
+//	public void systemet_opretter_et_nyt_projekt_og_tildeler_det_et_projekt_nummer_og_et_navn_Bruger_informeres_herom() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Given("<Bruger er logget ind>")
+//	public void bruger_er_logget_ind() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren opretter et nyt projekt>")
+//	public void brugeren_opretter_et_nyt_projekt() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren forsoeger at oprette et nyt projekt, paa et tidspunkt, hvor der stadig er ledige numre paa formaten|\\(y)aarstal, \\(n)loebenummer|: yynnn>")
+//	public void brugeren_forsoeger_at_oprette_et_nyt_projekt_paa_et_tidspunkt_hvor_der_stadig_er_ledige_numre_paa_formaten_y_aarstal_n_loebenummer_yynnn() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("<Brugeren angiver en projektleder>")
+//	public void brugeren_angiver_en_projektleder() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Then("<Systemet opretter et nyt projekt og tildeler det et projekt nummer og en leder. Dette nummer informeres brugeren om>")
+//	public void systemet_opretter_et_nyt_projekt_og_tildeler_det_et_projekt_nummer_og_en_leder_Dette_nummer_informeres_brugeren_om() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//	
+	
+}
