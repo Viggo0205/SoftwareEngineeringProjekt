28617ca71d098df09b38be34e95d632a0d4d2be3
clara <clara@192.168.0.101>
2020-05-05 23:22:21 +0200

    fis

--- test/ArbejdsdagTest.java
+++ test/ArbejdsdagTest.java
@@ -3,11 +3,44 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import SoftwareEngineringProjekt.src.Aktivitet;
+import SoftwareEngineringProjekt.src.Arbejdsdag;
+import SoftwareEngineringProjekt.src.Dato;
+
 public class ArbejdsdagTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@org.junit.Test
+	public void addTid() {
+    	Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(2020, 3));
+    	
+    	arbejdsdag.addTid("projekt", "aktivitet", 2);
+    	
+    	assertEquals(arbejdsdag.getTid(),arbejdsdag.getTid());
 	}
 
+	@org.junit.Test
+	public void getTid() {
+    	Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(2020, 3));
+    	
+    	arbejdsdag.addTid("projekt", "aktivitet", 2);
+    	
+    	assertEquals(arbejdsdag.getTid(),arbejdsdag.getTid());
+	}
+	
+	@org.junit.Test
+	public void findProjInd() {
+		Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(2020, 3);
+		
+		arbejdsdag.findProjInd(new projekt("test", 1));
+		
+		assertEquals(expected, arbejdsdag.findProjInd());
+	}
+	
+	
+//	@org.junit.Test
+//	public void findAktInd() {
+//		Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(2020, 2);	
+//	}
+//	
+	
 }
