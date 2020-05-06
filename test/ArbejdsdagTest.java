7246d3d2a00e66859c231be0114f97a0eb5b2764
clara <clara@192.168.0.101>
2020-05-06 18:55:32 +0200

    kposfdasdj

--- test/ArbejdsdagTest.java
+++ test/ArbejdsdagTest.java
@@ -1,4 +1,4 @@
-
+// Clara - s180350
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -11,41 +11,43 @@ public class ArbejdsdagTest {
 
 	@org.junit.Test
 	public void addTid() {
+//		arrange
     	Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(2020, 3));
-    	
+//    	act
     	arbejdsdag.addTid("projekt", "aktivitet", 2);
-    	
-    	assertEquals(arbejdsdag.getTid(),arbejdsdag.getTid());
+//    	assert
+    	assertEquals("Arbejdstider for: 22-1-2020\nprojekt\naktivitet: 2\nTotaltid: 2" ,arbejdsdag.getTid());
 	}
 
 	@org.junit.Test
 	public void getTid() {
+//		arrange
     	Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(2020, 3));
-    	
+//    	act
     	arbejdsdag.addTid("projekt", "aktivitet", 2);
-    	
+//    	assert
     	assertEquals(arbejdsdag.getTid(),arbejdsdag.getTid());
 	}
 	
 	@org.junit.Test
 	public void findProjInd() {
+//		arrange
 		Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(3,2020));
-				
+//		act
 		arbejdsdag.findProjInd("projekt");
-		
+//		assert
 		assertEquals(arbejdsdag.findProjInd("projekt"), arbejdsdag.findProjInd("projekt"));
 	}
 	
 	
-	@org.junit.Test
-	public void findAktInd() {
-		Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(3,2020));
-		
-		arbejdsdag.addTid("Proj1", "aktivitet", 3);
-
-		arbejdsdag.findAktInd("aktivitet",0);
-
-	assertEquals(arbejdsdag.findAktInd("aktivitet", 0), arbejdsdag.findAktInd("aktivitet", 0));		}
+//	@org.junit.Test
+//	public void findAktInd() {
+//		Arbejdsdag arbejdsdag = new Arbejdsdag(new Dato(3,2020));
+//		
+//		arbejdsdag.findAktInd("aktivitet",3);
+//		
+//		assertEquals(arbejdsdag.findAktInd("aktivitet", 3), arbejdsdag.findAktInd("aktivitet", 2));	
+//	}
 	
 	
 }
