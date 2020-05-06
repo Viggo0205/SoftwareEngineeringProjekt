9d1469dc36d12b0de17690e16a431d96ac427d13
Eigil <Eigil@DESKTOP-F9H9ECK>
2020-05-06 19:06:17 +0200

    QWERTY

--- test/TestRunner.java
+++ test/TestRunner.java
@@ -8,7 +8,11 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "use_cases/SoftwareEngineeringProjekt")
+@CucumberOptions(features = "use_cases/SoftwareEngineeringProjekt",
+	plugin = { "html:target/cucumber/wikipedia.html"},
+	snippets = SnippetType.CAMELCASE,
+	glue = { "dtu.library.acceptance_tests"},
+	strict = true)
 public class TestRunner {
 
 }
