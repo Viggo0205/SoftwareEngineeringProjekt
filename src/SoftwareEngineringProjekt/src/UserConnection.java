5ff5be6b733240149b3e6866784da402f286bb2f
Eigil <Eigil@DESKTOP-F9H9ECK>
2020-05-10 19:35:44 +0200

    Tilbage-fix

--- src/SoftwareEngineringProjekt/src/UserConnection.java
+++ src/SoftwareEngineringProjekt/src/UserConnection.java
@@ -102,19 +102,19 @@ public class UserConnection extends Thread {
 				}
 				else if (request[0].equals("1"))
 				{
-					sendBesked("1;" + ServControll.packagedProjTake2(request[1]));
+					sendBesked("1" + ServControll.packagedProjTake2(request[1]));
 				}
 				else if (request[0].equals("2"))
 				{
-					sendBesked("2;" + ServControll.packagedProjMedAktTake2(request[1]));
+					sendBesked("2" + ServControll.packagedProjMedAktTake2(request[1]));
 				}
 				else if (request[0].equals("3"))
 				{
-					sendBesked("3;" + ServControll.packagedMedarb());
+					sendBesked("3" + ServControll.packagedMedarb());
 				}
 				else if (request[0].equals("4"))
 				{
-					sendBesked("4;" + "TBD");
+					sendBesked("4" + "TBD");
 				}
 				else if (request[0].equals("a"))
 				{
