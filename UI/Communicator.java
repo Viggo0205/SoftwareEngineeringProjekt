b29d19212a6c56fe34ff7d5211c823cf4c9aad69
Rasmus Nyhus <Rasmus Nyhus@DESKTOP-PQHSS3Q>
2020-05-10 02:55:49 +0200

    eigil bad om push. ændret client til BiS og BoS

--- UI/Communicator.java
+++ UI/Communicator.java
@@ -5,7 +5,10 @@
  * så serveren kan læse det.
  */
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -14,8 +17,11 @@ import java.util.concurrent.TimeUnit;
 public class Communicator implements Runnable {
 
 	static Socket s = null;
-	static PrintWriter pw = null;
-	static Scanner netin = null;
+	static InputStream inps = null;
+	static DataInputStream dinp = null;
+	static DataOutputStream doutp = null;
+//	static PrintWriter pw = null;
+//	static Scanner netin = null;
 	public boolean comModRun = false;
 	private boolean listening = true;
 	private String[] ca = new String[2];
@@ -29,30 +35,48 @@ public class Communicator implements Runnable {
 		try {
 			s = new Socket("localhost", 8085);
 		} catch (IOException ioe) {
-			System.err.println("Socket not opened");
+			System.err.println("Server can't be reached");
 		}
 		
 		// in- og output initieres
 		try {
-			netin = new Scanner(s.getInputStream());
+			inps = s.getInputStream();
+			dinp = new DataInputStream(inps);
+
+//			netin = new Scanner(s.getInputStream());
 		} catch (IOException ioe) {
 			System.err.println("does socket s not exist?");
 		} catch (Exception e) {
 			System.err.println("netin not initiated");
 		}
 		try {
-			pw = new PrintWriter(s.getOutputStream());
+			doutp = new DataOutputStream ( s.getOutputStream() );
+			
+//			pw = new PrintWriter(s.getOutputStream());
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.err.println("pw not initiated");
 		}
 		
+		boolean moreData = false;
 		// loop, der løbende læser nye beskeder sendt fra serveren
 		while(comModRun) {
 			if (Controll.ready) {
-				if (netin.hasNext()) {	
-					ca = netin.nextLine().split(":",2);
-					Controll.msgQueue(ca);
+				try {
+					if (dinp.available() > 0)
+						moreData  = true;
+					else
+						moreData = false;
+				} catch (IOException e) { 
+					e.printStackTrace(); 
+				}
+				
+				if (moreData) {
+					try {
+						ca = dinp.readUTF().split(";",2);
+					} catch (IOException e) { 
+						e.printStackTrace(); 
+					}
 				}
 			}
 			try {
@@ -61,99 +85,85 @@ public class Communicator implements Runnable {
 				e.printStackTrace();
 			}
 		}
+		
 	}
 	
 	// sender ønske om login med brugerens initialer
-	public static void sendLogin(String initialer) {
-		pw.print("0;" + initialer);
-		pw.flush();
+	public static void sendLogin(String initialer) {	
+		sendBesked("0;" + initialer);
 	}
 	// sender besked om, klienten vil kende sine projekter
 	public static void sendProjAccess() {
-		pw.print("1");
-		pw.flush();
+		sendBesked("1");
 	}
 	// sender besked om, klienten vil kende sine projekter og aktiviteter
 	public static void sendAktivAccess() {
-		pw.print("2");
-		pw.flush();
+		sendBesked("2");
 	}
 	// sender forespørgsel om medarbejderliste
 	public static void sendMedarbAccess() {
-		pw.print("3");
-		pw.flush();
+		sendBesked("3");
 	}
 	public static void sendMedAktAccess() {
-		pw.print("4");
-		pw.flush();
+		sendBesked("4");
 	}
 	
 	// metoder for at sende beskeder til serveren afhængig af hvilket event.
 	public static void sendOpretAktiv(String projekt, String startUge, String slutUge, String timer) {
 		sendMsg = "a;" + projekt + ";" + startUge  + ";" + slutUge + ";" + timer;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendTildelAkt(String projekt, String aktivitet, String udvikler) {
 		sendMsg = "b;" + projekt + ";" + aktivitet + ";" + udvikler;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendSkafRapport(String projekt) {
 		sendMsg = "c;" + projekt;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendRegistrerFerie(String startDato, String slutDato) {
 		sendMsg = "d;" + startDato + ";" + slutDato;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendSoegHjaelp(String projekt, String aktivitet, String udvikler) {
 		sendMsg = "e;" + projekt + ";" + aktivitet + ";" + udvikler;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendRetterTimer(String dato, String timer) {
 		sendMsg = "f;" + dato + ";" + timer;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendOpretPro(String leder, String beskrivelse, String startDato, String slutDato) {
 		sendMsg = "g;" + leder + ";" + beskrivelse + ";" + startDato + ";" + slutDato;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendIndmeldTid(String projekt, String aktivitet, String dag, String timer) {
 		sendMsg = "h;" + projekt + ";" + aktivitet + ";" + dag + ";" + timer;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendErLedig(String uge) {
 		sendMsg = "i;" + uge;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendTidsbrug(String projekt, String aktivitet) {
 		sendMsg = "j;" + projekt + ";" + aktivitet;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
 	}
 	public static void sendSeArbejde(String startDato, String slutDato) {
 		sendMsg = "k;" + startDato + ";" + slutDato;
-		pw.print(sendMsg);
-		pw.flush();
-		sendMsg = null;
+		sendBesked(sendMsg);
+	}
+	
+	public static void sendBesked(String sendMessage) {
+		try {
+			doutp.writeUTF(sendMessage);
+			doutp.flush();
+			sendMsg = null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 
