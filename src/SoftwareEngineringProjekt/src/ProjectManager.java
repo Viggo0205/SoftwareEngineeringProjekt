22db3586ffc5af520cffbf4d4a2e135594a4109f
Eigil <Eigil@DESKTOP-F9H9ECK>
2020-05-10 19:44:26 +0200

    Kommentare

--- src/SoftwareEngineringProjekt/src/ProjectManager.java
+++ src/SoftwareEngineringProjekt/src/ProjectManager.java
@@ -54,7 +54,8 @@ public class ProjectManager {
 	}
 	
 	
-	//Pakker data til klienten
+			//Pakker data til klienten
+	//Pakker alle projekter til klienten
 	public String pakString() {
 		String s = "";
 		if(this.projekter.size() > 0)
@@ -67,6 +68,7 @@ public class ProjectManager {
 		return s;
 	}
 
+	//Pakker alle projekter til klienten, samt tilhørende aktiviteter
 	public String pakStringMedAkt() {
 		String s = "";
 		if(this.projekter.size() > 0)
@@ -82,6 +84,7 @@ public class ProjectManager {
 		return s;
 	}
 
+	//Pakker de projekter klienten selv er medlem af til vedkommende
 	public String pakStringTake2(String init) {
 		String s = "";
 		if(this.projekter.size() > 0)
@@ -95,6 +98,7 @@ public class ProjectManager {
 		return s;
 	}
 
+	//Pakker de projekter klienten er medlem af, samt tilhørende aktiviteter, til vedkommende
 	public String pakStringMedAktTake2(String init) {
 		String s = "";
 		if(this.projekter.size() > 0)
@@ -104,6 +108,40 @@ public class ProjectManager {
 				{
 					s += ";" + p.getProjektNummer() + "|" + p.getNavn();
 					if (p.getAktiviteter().size() > 0)
+						for (Aktivitet a : p.getAktiviteter())
+						{
+							if ( findMedarbVedInit(init, p, a) >= 0)
+							s += ":" + a.getNavn();
+						}
+				}
+			}
+		return s;
+	}
+	
+	// Pakker de projekter klienten er leder af til vedkommende
+	public String pakStringTake3(String init) {
+		String s = "";
+		if(this.projekter.size() > 0)
+			for (Project p : this.projekter)
+			{
+				if (p.getLeder().getInitialer().equals(init))
+					s += ";" + p.getProjektNummer() + "|" + p.getNavn();
+			}
+		else
+			s = ";";
+		return s;
+	}
+
+	//Pakker de projekter klienten er leder af, samt tilhørende aktiviteter, til vedkommende
+	public String pakStringMedAktTake3(String init) {
+		String s = "";
+		if(this.projekter.size() > 0)
+			for (Project p : this.projekter)
+			{
+				if (findMedarbVedInit(init, p) >= 0)
+				{
+					s += ";" + p.getProjektNummer() + "|" + p.getNavn();
+					if (p.getLeder().getInitialer().equals(init))
 						for (Aktivitet a : p.getAktiviteter())
 						{
 							if ( findMedarbVedInit(init, p, a) >= 0)
